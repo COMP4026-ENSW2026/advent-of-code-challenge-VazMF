e9577cc2bc1ea791e4186842750bd2175595ab29
VazMF <vazfernandam@gmail.com>
2023-02-17 19:56:52 -0300

    feat: adding day 6 part II solution

--- 01-advent-of-code-challenge/06/solution_part_ii.py
+++ 01-advent-of-code-challenge/06/solution_part_ii.py
@@ -1,1 +1,12 @@
 
+with open('01-advent-of-code-challenge/06/sample.in') as file:
+  buffer = [i for i in file.read().strip()]
+
+characters_number = 0
+
+for i in range(len(buffer)):
+  if len(set(buffer[i:i+14])) == 14:
+    characters_number = i+14
+    break
+
+print(f'É necessário processar {characters_number} caracteres.')
